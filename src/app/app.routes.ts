7f825fd20bf5b04426b15e5d0ef5ab709500f314
fravss <anaflaviamartins56@gmail.com>
2025-07-04 21:42:46 -0300

    adicionar imagem de fundo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { HeroisFormComponent } from './components/herois-form/herois-form.compon
 export const routes: Routes = [
      { path: 'heroi', component: HeroisTableComponent },
      { path: 'cadastrar', component: HeroisFormComponent},
-     { path: 'cadastrar/:id', component: HeroisFormComponent}
+     { path: 'cadastrar/:id', component: HeroisFormComponent},
+     { path: '', redirectTo: 'heroi', pathMatch: 'full' }
 
 ];
