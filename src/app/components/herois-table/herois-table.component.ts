5766c794bd0e4a6078f33dd274f6f27a7a8a481d
fravss <anaflaviamartins56@gmail.com>
2025-07-05 09:18:15 -0300

    adicionar paginator

--- src/app/components/herois-table/herois-table.component.ts
+++ src/app/components/herois-table/herois-table.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Herois } from '../../interfaces/herois';
 import { HeroisService } from '../../services/herois.service';
 import { Router } from '@angular/router';
@@ -7,10 +7,11 @@ import { DatePipe } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ToastService } from '../toast/toast.service';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-herois-table',
-  imports: [MatTableModule, MatIconModule, MatButtonModule],
+  imports: [MatTableModule, MatIconModule, MatButtonModule, MatPaginatorModule],
   templateUrl: './herois-table.component.html',
   styleUrl: './herois-table.component.scss',
   providers: [DatePipe]
@@ -18,6 +19,9 @@ import { ToastService } from '../toast/toast.service';
 export class HeroisTableComponent implements OnInit {
   heroisData: Herois[] = [];
   colunas: string[] = ['Id', 'Nome Real', 'Nome de Herói', 'Data de Nascimento', 'Peso', 'Altura', 'Acoes'];
+  dataSource!: MatTableDataSource<any>;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
   constructor(
     private heroiService: HeroisService,
     private router: Router,
@@ -30,14 +34,21 @@ export class HeroisTableComponent implements OnInit {
   }
 
   async getTodosHerois(): Promise<void> {
+  try {
     const data: Herois[] = await this.heroiService.getHerois();
-
+    
     this.heroisData = data.map(heroisData => ({
       ...heroisData,
       dataNascimento: this.datePipe.transform(heroisData.dataNascimento, 'dd/MM/yyyy') || '',
     }));
 
+    this.dataSource = new MatTableDataSource(this.heroisData);
+    this.dataSource.paginator = this.paginator;
+
+  } catch (error) {
+    this.toastService.callErrorToast('Erro ao buscar heróis.');
   }
+}
   alterarHeroi(id: number): void {
     this.router.navigate([`cadastrar/${id}`]);
   }
